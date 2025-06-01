0e5011ae157ceea62281897c8e9511d44c183ae5
Jisan901 <jisuna901@gmail.com>
2025-06-01 17:02:13 +0600

    added top await

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,11 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import htuiPlugin from './src/Application/Utils/vite-plugin-htui'
 import wasm from "vite-plugin-wasm"
+import topLevelAwait from 'vite-plugin-top-level-await'
+
 export default defineConfig({
   plugins: [
+    topLevelAwait(),
     tailwindcss(),
     htuiPlugin(),
     wasm()
